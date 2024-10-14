eacea104dd10ca19679fc5dca3ec4b17f52fc02f
joannegton <wtg131998@gmail.com>
2024-10-14 16:05:10 -0300

    update gerente

--- Alpha - Beaba/js/gerente.js
+++ Alpha - Beaba/js/gerente.js
@@ -14,12 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
     mostrarpedidoTaloes = () => {
         document.getElementById('pedidoTaloes').style.display = 'block'
         document.getElementById('relatorios').style.display = 'none'
+        document.getElementById('perfilAcesso').style.display = 'none'
         mostrarMenu()
     }
 
     mostrarRelatorios = () => {
         document.getElementById('pedidoTaloes').style.display = 'none'
         document.getElementById('relatorios').style.display = 'block'
+        document.getElementById('perfilAcesso').style.display = 'none'
+        mostrarMenu()
+    }
+
+    mostrarPerfilAcesso = () => {
+        document.getElementById('perfilAcesso').style.display = 'block'
+        document.getElementById('pedidoTaloes').style.display = 'none'
+        document.getElementById('relatorios').style.display = 'none'
         mostrarMenu()
     }
 
@@ -27,38 +36,46 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('modalPedido').style.display = 'block'
     }
 
+    // Perfis de acesso
+    const perfil = document.getElementById('mostrarPerfis')
+    const cadastroPerfil = document.getElementById('mostrarCadastro')
 
-
-
-
-    const entradas = document.getElementById('mostrarEntradas')
-    const saidas = document.getElementById('mostrarSaidas')
-    const indicador = document.querySelector('.indicador')
-
-    function toggleActive(elementToActivate, elementToDeactivate) {
-        elementToActivate.classList.add('ativo')
-        elementToActivate.classList.remove('inativo')
-        elementToDeactivate.classList.add('inativo')
-        elementToDeactivate.classList.remove('ativo')
-
-        if (elementToActivate === entradas) {
-            indicador.style.transform = 'translateX(0)'
-        } else {
-            indicador.style.transform = 'translateX(100%)'
-        }
-    }
-
-    entradas.addEventListener('click', function() {
-        toggleActive(entradas, saidas)
-        document.getElementById('entradas').style.display = 'block'
-        document.getElementById('saidas').style.display = 'none'
+    perfil.addEventListener('click', () => {
+        alternador(perfil, perfil, cadastroPerfil, 'seletorPerfis', 'seletorCadastro', 'indicadorPerfil')
     })
+
+    cadastroPerfil.addEventListener('click', ()=> {
+        alternador(cadastroPerfil, perfil, cadastroPerfil, 'seletorCadastro', 'seletorPerfis', 'indicadorPerfil')
+    })
+
+    // Relatórios
+    const saidas = document.getElementById('mostrarSaidas')
+    const entradas = document.getElementById('mostrarEntradas')
 
     saidas.addEventListener('click', function() {
-        toggleActive(saidas, entradas)
-        document.getElementById('saidas').style.display = 'block'
-        document.getElementById('entradas').style.display = 'none'
+        alternador(saidas, saidas, entradas, 'saidas', 'entradas', 'indicador')
     })
 
-    toggleActive(saidas, entradas)
+    entradas.addEventListener('click', function() {
+        alternador(entradas, saidas, entradas, 'entradas', 'saidas', 'indicador')
+    })
+
+
+
+
+    // geral
+    function alternador(elementoAtivo, elementoDesativar, elementoAtivar, ativar, desativar, indicador) {
+        elementoDesativar.classList.add('ativo')
+        elementoDesativar.classList.remove('inativo')
+        elementoAtivar.classList.add('inativo')
+        elementoAtivar.classList.remove('ativo')
+
+        if (elementoDesativar === elementoAtivo) {
+            document.getElementById(indicador).style.transform = 'translateX(0)'
+        } else {
+            document.getElementById(indicador).style.transform = 'translateX(100%)'
+        }
+        document.getElementById(ativar).style.display = 'grid'
+        document.getElementById(desativar).style.display = 'none'
+    }
 })
