8abd60d0ef19f00fa1e73c2de512a4ac8bf3cbb5
joannegton <wtg131998@gmail.com>
2024-10-31 13:40:49 -0300

    update main admin

--- alpha_Beaba/frontEnd/public/js/adm/main.js
+++ alpha_Beaba/frontEnd/public/js/adm/main.js
@@ -2,10 +2,10 @@ import {  exportarManutencao, filtarLojaManutencao, filtarStatusManutencao, most
 import { alternadorRelatorios, exportarRelatorios, iconeEstoqueBaixo, mostrarRelatorios } from './relatorios.js'
 import { mostrarMenu, logout, esconderElementos, mostrarFiltros } from '../../utils.js'
 import { mostrarLojas, ordenarLoja, salvarLoja, exportarLojas } from './lojas.js'
-import { mostrarEstoque, filtrarLoja, exportarEstoque } from './estoque.js';
+import { mostrarEstoque, filtrarLoja, exportarEstoque } from './estoque.js'
 import { enviarTalao, mostrarEnvioTaloes } from './envioTaloes.js'
-import { mostrarModalCadastroPerfil, mostrarPerfil, mostrarPerfilUsuario, salvarEditarPerfil, salvarPerfil } from './perfil.js';
-import { filtrarUsuarioNome, ordenarLojaUsuarios, ordenarUsuarios, salvarUsuario } from './usuarios.js';
+import { mostrarModalCadastroPerfil, mostrarPerfil, mostrarPerfilUsuario, salvarEditarPerfil, salvarPerfil } from './perfil.js'
+import { filtrarUsuarioNome, ordenarLojaUsuarios, ordenarUsuarios, salvarUsuario } from './usuarios.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     // Identificar primeiro Acesso
@@ -15,24 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Menu navigation
-
     if(window.innerWidth > 768){
         document.getElementById('menu').style.display = 'block'
     }
-    document.getElementById('mostrarEnvioTaloes').addEventListener('click', () =>{
-        mostrarEnvioTaloes()
-    })
+
+    document.getElementById('mostrarEnvioTaloes').addEventListener('click', mostrarEnvioTaloes)
     document.getElementById('mostrarEstoque').addEventListener('click', mostrarEstoque)
     document.getElementById('mostrarManutencao').addEventListener('click', mostrarManutencao)
-    document.getElementById('mostrarPerfil').addEventListener('click', () => {
-        mostrarPerfil()
-    })
-    document.getElementById('mostrarLojas').addEventListener('click', () => {
-        mostrarLojas()
-    })
-    document.getElementById('mostrarRelatorio').addEventListener('click', () => {
-        mostrarRelatorios()
-    })
+    document.getElementById('mostrarPerfil').addEventListener('click', mostrarPerfil)
+    document.getElementById('mostrarLojas').addEventListener('click', mostrarLojas)
+    document.getElementById('mostrarRelatorio').addEventListener('click', mostrarRelatorios)
+
+    // Card Usuario
     document.getElementById('usuario-info').addEventListener('click', mostrarPerfilUsuario)
     document.getElementById('sair-usuario').addEventListener('click', logout)
 
@@ -82,11 +76,6 @@ document.addEventListener('DOMContentLoaded', () => {
         esconderElementos(['addPerfil'])
     })
 
-    //document.getElementById('editarPerfis').addEventListener('click', editarPerfil)
-
-    //document.getElementById('deletarPerfis').addEventListener('click', deletarPerfil)
-
-    //document.getElementById('visualizarPermissoes').addEventListener('click', modalVisualizarPermissoes)
     document.getElementById('fecharModalVisualizarPermissoes').addEventListener('click', () => {
         esconderElementos(['modalVisualizarPermissoes'])
     })
@@ -107,11 +96,9 @@ document.addEventListener('DOMContentLoaded', () => {
         salvarLoja()
     })   
 
-
     // Relatórios
     document.getElementById('exportarRelatorios').addEventListener('click', exportarRelatorios)
     
-
 
     // Mostrar menu e filtros para mobile
     window.mostrarMenu = mostrarMenu
