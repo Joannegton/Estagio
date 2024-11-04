a99bf1c35bf35bfa3cd6a78e3b8dde59f5697492
joannegton <wtg131998@gmail.com>
2024-11-04 18:57:01 -0300

    padronização

--- alpha_Beaba/frontEnd/public/js/adm/relatorios.js
+++ alpha_Beaba/frontEnd/public/js/adm/relatorios.js
@@ -33,7 +33,7 @@ function carregarDadosRelatorios() {
     ])
 }
 
-function renderizartabelaEstoqueBaixo(){
+async function renderizartabelaEstoqueBaixo(){
     const tabelaEstoqueBaixo = document.getElementById('corpoTabelaEstoqueBaixo')
     tabelaEstoqueBaixo.innerHTML = ''
     
